b224a112f2e46a26c7cb828393805f0df0c4b4fa
predapaula4 <predapaula@yahoo.com>
2023-06-26 19:14:22 +0300

    fixed something

--- app/src/main/java/com/example/medical_appointments_application/authentification/SignInFragment.java
+++ app/src/main/java/com/example/medical_appointments_application/authentification/SignInFragment.java
@@ -9,9 +9,6 @@ import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
